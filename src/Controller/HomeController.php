467a244949096a9729ce6be30a18e5ced634a880
KCVenus <114671356+KCVenus@users.noreply.github.com>
2024-03-21 13:11:43 +0100

    resoudre boucle for qui boucle pas

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,21 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\ApiService;
 
+use App\Repository\AtelierRepository;
+use App\Repository\ThemeRepository;
+use App\Repository\VacationRepository;
+use App\Repository\HotelRepository;
+
+
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
-       return $this->render("home/home.html.twig");
-       
+    #[Route('/home', name: 'app_acceuil')]
+    public function index(AtelierRepository $atelierRepo, ThemeRepository $themeRepo, VacationRepository $vacationRepo, HotelRepository $hotelRepo): Response {
+        $ateliers = $atelierRepo->findAll();
+        $themes = $themeRepo->findAll();
+        $vacation = $vacationRepo->findAll();
+        $hotels = $hotelRepo->findAll();
+        
+        return $this->render('home/home.html.twig', [
+            'ateliers' => $ateliers,
+            'themes'   => $themes,
+            'vacation' => $vacation,
+            'hotels'   => $hotels 
+        ]);           
     }
-
-    
+   
     #[Route('/club/{id}', name: 'app_club_id')]
     public function getClub($id, ApiService $des): Response
     {
         $club = $des->getClub($id);
-        return new Response($club->getNom());
-               
+        return new Response($club->getNom());     
     }
 }
