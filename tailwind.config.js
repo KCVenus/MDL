ddf4ee8bb5b03f5c258850d070f4a85e2c62cc98
KCVenus <114671356+KCVenus@users.noreply.github.com>
2024-05-03 15:17:35 +0200

    retour css fabio

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+         colors: {
+        'bluePerso': '##B0C4DE',
+      },
+    },
   },
   plugins: [],
 }
